80abc40a8b5754cea1aadba826cded3acd6d2944
Jessie Ssebuliba <jessiessebuliba@gmail.com>
2025-08-08 17:27:28 +0300

    more fix up
    Signed-off-by: Jessie Ssebuliba <jessiessebuliba@gmail.com>

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,1 +1,5 @@
-// This file is deleted as per the updates.
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json("this will be the sitemap.xml route");
+}
