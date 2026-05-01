4b215ab58f4e3ee148eb16da357807ac583916c8
Jessie Ssebuliba <jessiessebuliba@gmail.com>
2026-05-01 13:38:09 +0300

    add my project
    Signed-off-by: Jessie Ssebuliba <jessiessebuliba@gmail.com>

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -3,92 +3,17 @@ import { WorkProject } from "../components/FolioItem";
 export const projects: WorkProject[] = [
   {
     id: "01",
-    title: "Retro Camera.",
-    category: "Website",
-    image: "images/portfolio/fuji.jpg",
-    image2x: "images/portfolio/fuji@2x.jpg",
+    title: "DTRUST",
+    category: "DApp",
+    image: "images/portfolio/dtrust.jpeg",
+    image2x: "images/portfolio/dtrust.jpeg",
     projectUrl: "#",
     modal: {
-      galleryImage: "images/portfolio/gallery/g-fuji.jpg",
+      galleryImage: "images/portfolio/dtrust.jpeg",
       description:
-        "Odio soluta enim quos sit asperiores rerum rerum repudiandae cum. Vel voluptatem alias qui assumenda iure et expedita voluptatem. Ratione officiis quae.",
-      tags: ["Branding", "Product Design"],
-      linkUrl: "https://www.behance.net/",
-    },
-  },
-  {
-    id: "02",
-    title: "The White Lamp.",
-    category: "Product Design",
-    image: "images/portfolio/lamp.jpg",
-    image2x: "images/portfolio/lamp@2x.jpg",
-    projectUrl: "#",
-    modal: {
-      galleryImage: "images/portfolio/gallery/g-lamp.jpg",
-      description:
-        "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.",
-      tags: ["Branding"],
-      linkUrl: "https://www.behance.net/",
-    },
-  },
-  {
-    id: "03",
-    title: "Rucksuck.",
-    category: "Branding",
-    image: "images/portfolio/rucksack.jpg",
-    image2x: "images/portfolio/rucksack@2x.jpg",
-    projectUrl: "#",
-    modal: {
-      galleryImage: "images/portfolio/gallery/g-rucksack.jpg",
-      description:
-        "Exercitationem reprehenderit quod explicabo consequatur aliquid ut magni ut. Deleniti quo corrupti illum velit eveniet ratione necessitatibus ipsam mollitia.",
-      tags: ["Product Design"],
-      linkUrl: "https://www.behance.net/",
-    },
-  },
-  {
-    id: "04",
-    title: "Since Day One.",
-    category: "Website",
-    image: "images/portfolio/skaterboy.jpg",
-    image2x: "images/portfolio/skaterboy@2x.jpg",
-    projectUrl: "#",
-    modal: {
-      galleryImage: "images/portfolio/gallery/g-skaterboy.jpg",
-      description:
-        "Dolores velit qui quos nobis. Aliquam delectus voluptas quos possimus non voluptatem voluptas voluptas. Est doloribus eligendi porro doloribus voluptatum.",
-      tags: ["Website"],
-      linkUrl: "https://www.behance.net/",
-    },
-  },
-  {
-    id: "05",
-    title: "Sand Dunes.",
-    category: "Illustration",
-    image: "images/portfolio/sanddunes.jpg",
-    image2x: "images/portfolio/sanddunes@2x.jpg",
-    projectUrl: "#",
-    modal: {
-      galleryImage: "images/portfolio/gallery/g-sanddunes.jpg",
-      description:
-        "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.",
-      tags: ["Illustration"],
-      linkUrl: "https://www.behance.net/",
-    },
-  },
-  {
-    id: "06",
-    title: "Minimalismo.",
-    category: "Branding",
-    image: "images/portfolio/minimalismo.jpg",
-    image2x: "images/portfolio/minimalismo@2x.jpg",
-    projectUrl: "#",
-    modal: {
-      galleryImage: "images/portfolio/gallery/g-minimalismo.jpg",
-      description:
-        "Quisquam vel libero consequuntur autem voluptas. Qui aut vero. Omnis fugit mollitia cupiditate voluptas. Aenean sollicitudin, lorem quis bibendum auctor.",
-      tags: ["Branding", "Product Design"],
-      linkUrl: "https://www.behance.net/",
+        "A decentralized document verification platform built on the Hedera Hashgraph network. Dtrust enables organizations to anchor cryptographic proofs of their documents onto the Hedera Consensus Service (HCS), creating an immutable, tamper-proof record that can be verified by anyone, anywhere, without compromising privacy.",
+      tags: ["Trustless", "Hedera", "DApp"],
+      linkUrl: "https://github.com/Jexsie/dtrust",
     },
   },
 ];
